31a75bcfd96a0c031812e8cf689b9544d732e024
Sean Nary <sean04apple@gmail.com>
2026-02-26 00:57:16 -0500

    prod db connection fix

--- backend/app/database.py
+++ backend/app/database.py
@@ -23,7 +23,7 @@ if not DATABASE_URL:
 if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
     DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://", 1)
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
