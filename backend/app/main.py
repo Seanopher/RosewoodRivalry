fb93155d1245475c00b7870f12d2f7781e10df1a
Sean Nary <sean04apple@gmail.com>
2026-02-26 01:13:19 -0500

    render change

--- backend/app/main.py
+++ backend/app/main.py
@@ -98,6 +98,7 @@ if frontend_url:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.onrender\.com",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
